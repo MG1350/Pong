b4c8f3abf09179cf3d6a2b844972ec6e9580fec5
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-05-20 14:58:01 -0400

    .

---
+++ PongBall.java
@@ -0,0 +1,35 @@
+public class PongBall 
+{
+    private int x;
+    private int y;
+    private int xVelocity;
+    private int yVelocity;
+    public PongBall(int initX, int initY, int initXVel, int initYVel)
+    {
+        x = initX;
+        y = initY;
+        xVelocity = initXVel;
+        yVelocity = initYVel;
+    }
+    public void bounceX()
+    {
+        xVelocity = xVelocity * -1;
+    }
+    public void bounceY()
+    {
+        yVelocity = yVelocity * -1;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public void move()
+    {
+        x += xVelocity;
+        y += yVelocity;
+    }
+}
